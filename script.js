80f4a355143dddb10a9fa2b3731315cf95f005e7
Rakshith <rakshithrkumar9161@gmail.com>
2024-01-03 17:49:56 +0530

    added thala for a reason

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const add = (function(){
     let counter = 0;
     function increment(){
         counter +=1;
+        if(counter == 7){
+            document.getElementById('thala').innerHTML = "Thala for a reason";
+        }
+        else{
+            document.getElementById('thala').innerHTML = "";
+        }
         return counter;
     }
    
